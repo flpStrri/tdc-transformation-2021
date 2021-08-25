fafb62bd473a60f0a6ec12263c571ad0b9d9f059
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2021-08-25 07:44:47 -0300

    Remove architecture clutter for the sake of simplicity

--- src/application/foo_repository.py
+++
@@ -1,8 +0,0 @@
-from typing import Protocol
-
-from domain.foo import Foo
-
-
-class FooRepository(Protocol):  # pragma: no cover
-    async def load_foo(self, identifier: str) -> Foo:
-        pass
--- src/presentation/rest_api/main.py
+++ src/presentation/rest_api/main.py
@@ -1,14 +1,15 @@
 import logging
+from uuid import uuid4
 
+import bcrypt
 from aiodynamo.errors import ItemNotFound
 from fastapi import FastAPI, HTTPException
 from fastapi.logger import logger
 from fastapi.responses import JSONResponse
 
-from application.foo_service import FooService
+from domain.foo import Foo
 from infrastructure.aiohttp import get_aiohttp_client
 from infrastructure.database import data_table
-from infrastructure.foo_repository import FooRepository
 
 app = FastAPI()
 fast_api_logger = logger
@@ -17,10 +18,24 @@ fast_api_logger = logger
 @app.get("/foos/{foo_id}")
 async def get_foo(foo_id: str):
     async with get_aiohttp_client() as client:
-        foo_service = FooService(foo_repository=FooRepository(data_table(client)))
+        dynamodb_table = data_table(client)
+        request_id = uuid4().hex
+        logger.info(f"{request_id} >> before loading foos/{foo_id}")
         try:
-            loaded_foo = await foo_service.load_foo(identifier=foo_id)
-            return JSONResponse(loaded_foo.dict())
+            logger.info(f"{request_id}     >> I/O work on foos/{foo_id}")
+            foo_table_item = await dynamodb_table.get_item({"pk": foo_id, "sk": "A"})
+            logger.info(f"{request_id}     << I/O work on foos/{foo_id}")
+
+            logger.info(f"{request_id}     >> before CPU foos/{foo_id}")
+            bcrypt.hashpw(foo_table_item["title"].encode("utf-8"), bcrypt.gensalt(6))
+            decoded_foo = Foo(
+                identifier=foo_table_item["pk"],
+                title=foo_table_item["title"],
+            )
+            logger.info(f"{request_id}     << after CPU foos/{foo_id}")
+
+            logger.info(f"{request_id} << after loading foos/{foo_id}")
+            return JSONResponse(decoded_foo.dict())
         except ItemNotFound:
             raise HTTPException(status_code=404, detail="foo not found")
 
